90351e9d821bcc2e487503d19e49039a135bf9fc
CodingSix <codingsix@gmail.com>
2016-07-10 15:18:47 +0800

    首页 header
    初步效果 暂无改样式

---
+++ js/index.js
@@ -0,0 +1,41 @@
+$(function(){
+    //页面滚动事件
+    window.onscroll=function(){
+        var osTop=document.documentElement.scrollTop || document.body.scrollTop;
+        if(osTop>50){
+            var top=osTop-50;
+            $(".banner-bg").css({
+                backgroundPosition:"0 -"+ top + "px"
+            });
+        }
+        
+        
+        if(osTop>200){
+            $("#header").css({
+               backgroundColor:"#fff",
+            });
+
+            $(".header-item").css({
+                color:"#000"
+            });
+            
+            $(".register").css({
+                color:"#fff"
+            });   
+
+            $("#header").addClass("addstyle");
+            
+        }
+        else{
+            $("#header").removeClass("addstyle");
+            $(".header-item").css({
+                color:"#fff"
+            });
+             $("#header").css({
+               backgroundColor:"transparent",
+            });
+
+        }
+
+    }
+})
